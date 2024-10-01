09003e2ab32ac14b2a40f7799217c7fb809b5d15
Yukiko Asai <asai@ecdb.jp>
2024-10-01 11:43:03 +0900

    ca_fileを指定しない

--- common/lib/azure/storage/common/core/http_client.rb
+++ common/lib/azure/storage/common/core/http_client.rb
@@ -49,34 +49,36 @@ module Azure::Storage::Common::Core
 
     private
 
-      # Empties all information that cannot be reused.
-      def reuse_agent!(agent)
-        agent.params.clear
-        agent.headers.clear
-      end
+    # Empties all information that cannot be reused.
+    def reuse_agent!(agent)
+      agent.params.clear
+      agent.headers.clear
+    end
 
-      def build_http(uri)
-        ssl_options = {}
-        if uri.is_a?(URI) && uri.scheme.downcase == "https"
-          ssl_options[:version] = self.ssl_version if self.ssl_version
-          # min_version and max_version only supported in ruby 2.5
-          ssl_options[:min_version] = self.ssl_min_version if self.ssl_min_version
-          ssl_options[:max_version] = self.ssl_max_version if self.ssl_max_version
-          ssl_options[:ca_file] = self.ca_file if self.ca_file
-          ssl_options[:verify] = true
-        end
-        proxy_options = if ENV["HTTP_PROXY"]
-                          URI::parse(ENV["HTTP_PROXY"])
-                        elsif ENV["HTTPS_PROXY"]
-                          URI::parse(ENV["HTTPS_PROXY"])
-                        end || nil
-        Faraday.new(uri, ssl: ssl_options, proxy: proxy_options) do |conn|
-          conn.use FaradayMiddleware::FollowRedirects
-          conn.adapter :net_http_persistent, pool_size: 5 do |http|
-            # yields Net::HTTP::Persistent
-            http.idle_timeout = 100
-          end
+    def build_http(uri)
+      ssl_options = {}
+      if uri.is_a?(URI) && uri.scheme.downcase == "https"
+        ssl_options[:version] = self.ssl_version if self.ssl_version
+        # min_version and max_version only supported in ruby 2.5
+        ssl_options[:min_version] = self.ssl_min_version if self.ssl_min_version
+        ssl_options[:max_version] = self.ssl_max_version if self.ssl_max_version
+        # ca_fileを指定しない
+        #ssl_options[:ca_file] = self.ca_file if self.ca_file
+        ssl_options[:verify] = true
+      end
+      proxy_options =
+        if ENV["HTTP_PROXY"]
+          URI.parse(ENV["HTTP_PROXY"])
+        elsif ENV["HTTPS_PROXY"]
+          URI.parse(ENV["HTTPS_PROXY"])
+        end || nil
+      Faraday.new(uri, ssl: ssl_options, proxy: proxy_options) do |conn|
+        conn.use FaradayMiddleware::FollowRedirects
+        conn.adapter :net_http_persistent, pool_size: 5 do |http|
+          # yields Net::HTTP::Persistent
+          http.idle_timeout = 100
         end
       end
+    end
   end
 end
